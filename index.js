0858cddcb0aff60e3717262010f88faa269042df
unknown <jaideepgrover147@gmail.com>
2022-01-08 16:11:41 +0530

    admin link

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ let port = process.env.PORT || 3002
 
 app.use(cors({
     credentials: true,
-    origin: ['https://cycle-master-ed965.web.app', 'https://cycle-master-new-6b4ef.web.app/']
+    origin: ['https://cycle-master-ed965.web.app', 'https://cycle-admin-3282.web.app']
 }));
 app.use(cookieParser())
 app.use(bodyParser.json({limit: '100mb'}))
